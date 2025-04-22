a15388c1c9363006b38f8549f4fb897865dc8f93
Guillem Garcia <garcia.guillem.dausa@gmail.com>
2025-04-22 23:53:20 +0200

    centralities

--- code/metrics.py
+++ code/metrics.py
@@ -40,7 +40,15 @@ class Metrics:
         # print(f"Union: {union}")
         return len(union)
 
-    def jaccardCoefficient(self, g1, g2):
+    def jaccardIndex(self, g1, g2):
+        """Calcular el coeficient de Jaccard entre dos grafs (edge overlap)
+
+        Args:
+            g1, g2 (nx.Graph): Grafs a calcular el coeficient de Jaccard
+
+        Returns:
+            float: Coeficient de Jaccard entre els dos grafs, valor en percentatge
+        """
         # En cas de no ser dirigit els grafs, s'ha de descartar les arestes duplicades
         if g1.is_directed():
             edgesG1 = set(g1.edges())
@@ -63,7 +71,7 @@ class Metrics:
         # Calculem el coeficient de Jaccard i el retornem en forma de percentatge
         return (intersection / union)*100
 
-    def degreeMatrices(self, graph):
+    def degreeMatrices(self, graph, type):
         """Obtenir les matrius diagonals de graus del graf
 
         Args:
@@ -73,7 +81,12 @@ class Metrics:
             np.array, int: Matriu diagonal de graus, amb el grau màxim
         """
         # Obtenim les seqüències de grau per cada graf, i calculem el grau màxim
-        degreeDict1 = dict(graph.degree())
+        if graph.is_directed() and type == 'out':
+            degreeDict1 = dict(graph.out_degree())
+        elif graph.is_directed() and type == 'in':
+            degreeDict1 = dict(graph.in_degree())
+        else:
+            degreeDict1 = dict(graph.degree())
         degreeSequence1 = [degreeDict1.get(node, 0) for node in graph.nodes()]
         maxDegree1 = max(degreeSequence1)
 
@@ -93,7 +106,7 @@ class Metrics:
         """
         return 1 / (1+maxDegree)
 
-    def scoreMatrix(self, graph):
+    def scoreMatrix(self, graph, type):
         """Calcular la matriu d'influència de nodes d'un graf
 
         Args:
@@ -104,7 +117,7 @@ class Metrics:
         """
         identityMatrix = np.identity(graph.number_of_nodes())
         # print(f"Identity Matrix: {identityMatrix}")
-        degreeMatrix, maxDegree = self.degreeMatrices(graph)
+        degreeMatrix, maxDegree = self.degreeMatrices(graph, type)
         adjacencyMatrix = nx.adjacency_matrix(graph).toarray()
         # print(f"Adjacency Matrix: {adjacencyMatrix}")
         influence = self.influenceNeighbors(maxDegree)
@@ -129,16 +142,66 @@ class Metrics:
         distance = np.sqrt(total_sum)
         return distance
 
-    def deltaConnectivity(self, g1, g2):
-        pass
-    
-        
-    def spectralSimilarity(self, g1, g2):
-        pass
+    def deltaConnectivity(self, g1, g2, type):
+        """Obtenir la similaritat per l'afinitat entre nodes de dos grafs
 
-    def centralityCorrelation(self, g1, g2, function):
-        pass
+        Args:
+            g1, g2 (nx.Graph): Grafs a calcular la seva similaritat
+
+        Returns:
+            float: Similaritat entre els dos grafs (afinitat), valor en percentatge
+        """
+        S1 = self.scoreMatrix(g1, type)
+        S2 = self.scoreMatrix(g2, type)
+        # print(f"S1: {S1}, S2: {S2}")
+        distance = self.rootEuclideanDistance(S1, S2)
+        # print(f"Distance: {distance}")
+        return (1/(1+distance))*100
+        
+    def getDensities(self, g1, g2):
+        """Obtenir les densitats dels grafs originals i protegits
+
+        Args:
+            g1, g2 (nx.Graph): Grafs a calcular la seva densitat
+
+        Returns:
+            List, List: Llista de densitats dels grafs originals i protegits
+        """
+        densityOriginal = [nx.density(g) for g in g1]
+        densityProtected = [nx.density(g) for g in g2]
+        return densityOriginal, densityProtected
     
+    def topKNodes(centralityDict, k):
+        """Obtenir els k nodes més centrals d'un graf
+
+        Args:
+            centrality_dict (Dict): Diccionari de centralitat dels nodes
+            k (int): nombre de nodes més centrals a obtenir
+
+        Returns:
+            set: Conjunt dels k nodes més centrals
+        """
+        return set(sorted(centralityDict, key=centralityDict.get, reverse=True)[:k])
+
+    def computeCentrality(self, g1, g2, function):
+        centrality1 = function(g1)
+        centrality2 = function(g2)
+
+        # Obtenim el 5% nodes més centrals de cada graf
+        k = max(1, int(0.05 * g1.number_of_nodes()))
+        topG1 = self.topKNodes(centrality1, k)
+        topG2 = self.topKNodes(centrality2, k)
+        
+        intersection = topG1 & topG2
+        union = topG1 | topG2
+        return len(intersection) / len(union)
+    
+    def getCentrality(self, g1, g2):
+        jaccardBC = self.computeCentrality(g1, g2, nx.betweenness_centrality)
+        jaccardCC = self.computeCentrality(g1, g2, nx.closeness_centrality)
+        jaccardDC = self.computeCentrality(g1, g2, nx.degree_centrality)
+        return jaccardBC, jaccardCC, jaccardDC
+        
     def computeMetrics(self):
         pass
 
