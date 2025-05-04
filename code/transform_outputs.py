eee468046f28796c9e8cdac990026682a0c788c8
Guillem Garcia <garcia.guillem.dausa@gmail.com>
2025-05-04 23:27:05 +0200

    convert graphs into txt files

---
+++ code/transform_outputs.py
@@ -0,0 +1,36 @@
+import os
+import pickle
+import data_paths as dp  
+
+def convertOutputsTxt():
+    """Converteix els grafs originals i protegits en format .txt
+    """
+    folders = dp.OUTPUTS
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    for root_folder in folders:
+        for dirpath, dirnames, filenames in os.walk(root_folder):
+            for file in filenames:
+                if not file.endswith('.pkl'):
+                    continue  
+
+                input_path = os.path.join(dirpath, file)
+
+                with open(input_path, 'rb') as f:
+                    graphList = pickle.load(f)
+
+                rel_path = os.path.relpath(dirpath, root_folder)
+
+                base_folder = os.path.basename(root_folder)
+                out_dir = os.path.join(current_dir, "data", "output", base_folder, rel_path)
+                os.makedirs(out_dir, exist_ok=True)
+
+                output_file = os.path.join(out_dir, file[:-4] + ".txt")
+
+                with open(output_file, mode='w', encoding='utf-8') as o:
+                    for timestamp, graph in enumerate(graphList):
+                        for u, v in graph.edges():
+                            o.write(f"{u} {v} {timestamp}\n")
+
+if __name__ == "__main__":
+    convertOutputsTxt()
